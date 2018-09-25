987ee65c568da4cc47adb4445345ffac54e3a1cc
1ktoto1 <43352108+1ktoto1@users.noreply.github.com>
2018-09-25 22:51:28 +0300

    first commit

---
+++ дз1.py
@@ -0,0 +1,4 @@
+num_1 = float (input ("Введіть 1 число: "))
+num_2 = float (input ("Введіть 2 число: "))
+num_3 = float (input ("Введіть 3 число: "))
+print ("Сума цих чисел дорівнює: ", num_3+num_2+num_1)
